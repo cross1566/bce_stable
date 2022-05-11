aec2a2d479b42abcaaa0c20157986234b32f6656
corss1566 <105389530+corss1566@users.noreply.github.com>
2022-05-11 14:20:45 -0700

    3.2.2

--- bce.user.js
+++ bce.user.js
@@ -39,7 +39,7 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const BCE_VERSION = "cross";
+const BCE_VERSION = "3.2.2";
 const settingsVersion = 35;
 
 const bceChangelog = `${BCE_VERSION}
